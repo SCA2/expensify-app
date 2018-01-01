a1c003c40477a2edb6c46d6196b9296e81c036d9
SCA2 <tpryan_1965@yahoo.com>
2017-12-31 16:59:53 -0800

    add expensify styles

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -5,16 +5,25 @@ import selectExpenses from '../selectors/expenses';
 
 
 export const ExpenseList = (props) => (
-  <div>
-    {
-      props.expenses.length === 0 ? (
-        <p>No expenses</p>
-      ) : (
-        props.expenses.map(e => {
-          return <ExpenseListItem key={e.id} {...e} />
-        })
-      )
-    }
+  <div className="content-container">
+    <div className="list-header">
+      <div className="show-for-mobile">Expenses</div>
+      <div className="show-for-desktop">Expense</div>
+      <div className="show-for-desktop">Amount</div>
+    </div>
+    <div className="list-body">
+      {
+        props.expenses.length === 0 ? (
+          <div className="list-item list-item-message">
+            <span>No expenses</span>
+          </div>
+        ) : (
+          props.expenses.map(e => {
+            return <ExpenseListItem key={e.id} {...e} />
+          })
+        )
+      }
+    </div>
   </div>
 );
 
--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
@@ -9,9 +10,15 @@ const pluralize = (noun, length) => (
 );
 
 export const ExpensesSummary = ({ expenseCount, expensesTotal }) => (
-  <h1>
-    {`Viewing ${expenseCount} ${pluralize('expense', expenseCount)} totalling ${numeral(expensesTotal / 100).format('$0,0.00')}`}
-  </h1>
+  <div className="page-header">
+    <div className="content-container">
+      <h1 className="page-header__title">Viewing <span>{expenseCount}</span> {pluralize('expense', expenseCount)} totalling <span>{numeral(expensesTotal / 100).format('$0,0.00')}</span>
+      </h1>
+      <div className="page-header__actions">
+        <Link className="login-button" to="/create">Add Expense</Link>
+      </div>
+    </div>
+  </div>
 );
 
 const mapStateToProps = state => {
